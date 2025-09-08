3a51f99ae5e85ba5b5e2edb1a35361d7581e9649
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-09-08 02:32:42 -0300

    q6

---
+++ q6.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main (){
+    int n;
+    
+    printf("Digite a quantidades de linhas: ");
+    scanf("%d", &n);
+
+    for (int i = n; i>=1; i--){
+        for (int j = 1; j <=i; j++)
+        {
+            printf("cu");
+        }
+        printf("\n");
+    }
+
+    system("PAUSE");
+    return 0;
+}
+
